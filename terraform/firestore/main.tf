c1336d9234b5549735ca08449e3f4af34aa8ac75
shamsmosowi <shams.mosowi@gmail.com>
2021-11-13 23:52:51 +1100

    add google project

--- terraform/firestore/main.tf
+++ terraform/firestore/main.tf
@@ -1,4 +1,9 @@
 
+
+resource "google_project" "gcp_project" {
+  name = var.project_id
+  project_id = var.project_id
+}
 resource "google_app_engine_application" "firestore" {
   project = var.project_id
   database_type = "CLOUD_FIRESTORE"
